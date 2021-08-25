053163056b9e1fa99684bf015dff285ee62ea534
thiagopazjs <thiagopaz907@gmail.com>
2021-08-25 12:58:29 -0300

    alteracoes JS html css inclusao bibliotecas

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*  abre e fecha o menu quando clicar no icone: hamburguer e x */
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
@@ -7,7 +8,7 @@ for (const element of toggle) {
   })
 }
 
-/*Ao clicar utem do menu, esconder o menu*/
+/* quando clicar em um item do menu, esconder o menu */
 const links = document.querySelectorAll('nav ul li a')
 
 for (const link of links) {
@@ -16,16 +17,44 @@ for (const link of links) {
   })
 }
 
-/*alterar header da página ao der scroll*/
+/* mudar o header da página quando der scroll */
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
 window.addEventListener('scroll', function () {
   if (window.scrollY >= navHeight) {
-    //Scroll é maior que a altura do header
+    // scroll é maior que a altura do header
     header.classList.add('scroll')
   } else {
-    //Scroll é MENOR que a altura do Header
+    // menor que a altura do header
     header.classList.remove('scroll')
   }
 })
+
+/* Testimonials carousel slider swiper */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)
